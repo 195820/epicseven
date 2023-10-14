08d684d22ef1f85cddac8daeaea059b226254913
应岚 <48042626+195820@users.noreply.github.com>
2023-10-14 16:30:06 +0800

    1

--- gameScript/gamescript.bat
+++ gameScript/gamescript.bat
@@ -1,6 +1,2 @@
-@echo off
-if "%1"=="h" goto begin
-start mshta vbscript:createobject("wscript.shell").run("""%~nx0"" h",0)(window.close)&&exit
-:begin
-::以下为正常批处理命令，不可含有pause set/p等交互命令
 python main.py
+pause
